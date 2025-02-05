6a1bba802e19206b7ea95f56f75f8929dc5c3edf
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2025-02-05 12:33:44 +0100

    Update Saver.py

--- src/Utils/Saver.py
+++ src/Utils/Saver.py
@@ -1,3 +1,10 @@
+# ------------------------------------------------------
+# This code is licensed under the MIT License.
+# Copyright (c) 2025 Martina Plumari, Daniel Bologna.
+# Developed for the course "Computational Intelligence" 
+# at Politecnico di Torino.
+# ------------------------------------------------------
+
 from Utils.ProblemLoader import Problem
 
 class Saver:

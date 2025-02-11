8a807c38e77a145453cfe284d80566e79655af4d
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2025-02-11 14:49:19 +0100

    Update s317612.py

--- s317612.py
+++ s317612.py
@@ -4,9 +4,29 @@
 
 import numpy as np
 
-def f5(x : np.ndarray) -> np.ndarray:
-	return np.log(np.divide(x[1], x[1]))
+def f0(x : np.ndarray) -> np.ndarray:
+	return np.add(x[0], np.multiply(x[1], 0.180848))
 
 def f1(x : np.ndarray) -> np.ndarray:
-	return np.cos(np.add(x[0], np.add(x[0], -8.02611)))
+	return np.sin(x[0])
 
+def f2(x : np.ndarray) -> np.ndarray:
+	return np.multiply(5.31454, np.multiply(np.divide(x[0], np.subtract(np.tan(x[1]), np.tan(x[1]))), 2.54036))
+
+def f3(x : np.ndarray) -> np.ndarray:
+	return np.multiply(x[1], np.subtract(x[1], np.multiply(x[1], x[1])))
+
+def f4(x : np.ndarray) -> np.ndarray:
+	return np.subtract(2.79429, np.multiply(-7.08441, np.cos(x[1])))
+
+def f5(x : np.ndarray) -> np.ndarray:
+	return np.subtract(np.multiply(np.sqrt(x[0]), np.sqrt(x[0])), x[0])
+
+def f6(x : np.ndarray) -> np.ndarray:
+	return np.subtract(x[0], np.multiply(np.subtract(x[0], x[1]), np.sqrt(3.28835)))
+
+def f7(x : np.ndarray) -> np.ndarray:
+	return np.multiply(x[1], np.multiply(np.subtract(np.divide(np.cos(-8.4597), x[1]), x[0]), -7.52244))
+
+def f8(x : np.ndarray) -> np.ndarray:
+	return np.divide(x[5], np.subtract(x[3], x[3]))

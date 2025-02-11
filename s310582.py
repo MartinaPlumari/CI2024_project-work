b8bb668aa27277271792d91b6504152af6c2cc21
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2025-02-11 11:12:56 +0100

    removed unnecessary files

--- s310582.py
+++
@@ -1,9 +0,0 @@
-# CI 2024 final project
-# Student Daniel Bologna
-# ID s310582
-
-import numpy as np
-
-def f7(x : np.ndarray) -> np.ndarray:
-	return np.multiply(x[1], np.multiply(np.subtract(np.divide(np.cos(-8.4597), x[1]), x[0]), -7.52244))
-
